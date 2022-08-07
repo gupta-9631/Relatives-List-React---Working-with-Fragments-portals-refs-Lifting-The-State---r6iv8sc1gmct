97d1ea7ce2a75c3c475ab5c95e448ba44de005ea
Rahul <rk255009@gmail.com>
2022-08-07 12:25:37 +0530

    added

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,54 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
 
-class App extends Component {
-    render() {
 
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-            </div>
-        )
-    }
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      RelativesList: [
+        {
+          Name: "Dhanu",
+        },
+        {
+          Name: "Mama",
+        },
+        {
+          Name: "Ravi",
+        },
+        {
+          Name: "Rohit",
+        },
+        {
+          Name: "Mausa",
+        },
+        {
+          Name: "Maousi",
+        },
+        {
+          Name: "Bua",
+        },
+        {
+          Name: "Vikash",
+        },
+        {
+          Name: "Raushan",
+        },
+      ],
+    };
+  }
+
+  render() {
+    return (
+      <div id="main">
+        <ol key="relativeList">
+          {this.state.RelativesList.map((item, index) => {
+            return <li key={"relativeListItem" + index + 1}>{item.Name}</li>;
+          })}
+        </ol>
+      </div>
+    );
+  }
 }
 
 
